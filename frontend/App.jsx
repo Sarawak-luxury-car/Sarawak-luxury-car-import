85efcf76a4121138db6c5a876c6e44c6cae72b76
Sarawak-luxury-car <funnyvideo0705@gmail.com>
2025-02-23 18:00:21 +0800

    Create App.jsx

---
+++ frontend/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Sarawak Luxury Car Import</h1>
+      <p>Welcome to the luxury car import platform.</p>
+    </div>
+  );
+}
